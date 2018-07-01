0cfcd68dbc5c1b9491412a5e8033a39421c908c1
Rafael Leles <r.lleles@gmail.com>
2018-06-30 21:44:41 -0300

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# ponto-inteligente-api-leles
+Estudo detalhada API restful
---
+++ src/main/java/br/com/pontointeligente/PontoInteligenteApiLelesApplication.java
@@ -0,0 +1,12 @@
+package br.com.pontointeligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoInteligenteApiLelesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoInteligenteApiLelesApplication.class, args);
+	}
+}
